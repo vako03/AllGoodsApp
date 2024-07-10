b85a573921cf98ed1a4bb11a4a537b4f4318a4a5
Vako <valeri.mekhashishvili97@gmail.com>
2024-07-10 20:26:14 +0400

    added tabbar logic

--- AllGoodsApp/ViewModel/Views/CustomComponents/CustomTabBarController.swift
+++ AllGoodsApp/ViewModel/Views/CustomComponents/CustomTabBarController.swift
@@ -7,13 +7,20 @@
 
 import UIKit
 
-class CustomTabBarController: UITabBarController {
+class CustomTabBarController: UITabBarController, UITabBarControllerDelegate {
+
     private var middleButton: UIButton!
+    private var previousSelectedIndex: Int?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         setupMiddleButton()
         customizeTabBarAppearance()
+        self.delegate = self  // Set the tab bar controller delegate
+
+        // Set initial selected item color to black
+        self.selectedIndex = 0  // Ensures the mainVC is the first loaded tab
+        updateTabBarItemsColor(selectedIndex: 0)
     }
 
     func setupMiddleButton() {
@@ -29,6 +36,7 @@ class CustomTabBarController: UITabBarController {
 
         middleButton.addAction(UIAction { [weak self] _ in
             self?.selectedIndex = 2  // Assuming ExpressViewController is at index 2
+            self?.updateTabBarItemsColor(selectedIndex: 2)
         }, for: .touchUpInside)
 
         view.addSubview(middleButton)
@@ -48,10 +56,36 @@ class CustomTabBarController: UITabBarController {
         let appearance = UITabBarAppearance()
         appearance.configureWithOpaqueBackground()
         appearance.backgroundColor = .white
+        appearance.stackedLayoutAppearance.normal.iconColor = .gray
+        appearance.stackedLayoutAppearance.normal.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.gray]
         appearance.stackedLayoutAppearance.selected.iconColor = .black
         appearance.stackedLayoutAppearance.selected.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.black]
 
         tabBar.standardAppearance = appearance
         tabBar.scrollEdgeAppearance = appearance
+    }
+
+    // Helper function to update tab bar item colors
+    func updateTabBarItemsColor(selectedIndex: Int) {
+        guard let items = tabBar.items else { return }
+
+        for (index, item) in items.enumerated() {
+            if index == selectedIndex {
+                item.image = item.image?.withTintColor(.black, renderingMode: .alwaysOriginal)
+                item.setTitleTextAttributes([NSAttributedString.Key.foregroundColor: UIColor.black], for: .normal)
+            } else {
+                item.image = item.image?.withTintColor(.gray, renderingMode: .alwaysOriginal)
+                item.setTitleTextAttributes([NSAttributedString.Key.foregroundColor: UIColor.gray], for: .normal)
+            }
+        }
+
+        previousSelectedIndex = selectedIndex
+    }
+
+    // UITabBarControllerDelegate method to handle tab selection
+    func tabBarController(_ tabBarController: UITabBarController, didSelect viewController: UIViewController) {
+        if let selectedIndex = tabBarController.viewControllers?.firstIndex(of: viewController) {
+            updateTabBarItemsColor(selectedIndex: selectedIndex)
+        }
     }
 }
